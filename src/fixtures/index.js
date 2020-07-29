f78e296243b6367bcd194ec7d3c6caffd43ddf7a
Tommy Cao <gevh2880@gmail.com>
2020-07-28 21:45:38 -0700

    first commit

---
+++ src/fixtures/index.js
@@ -0,0 +1,36 @@
+// Name: Tommy Cao
+// Date: 12/12/19
+// Description: React Enzyme testing
+
+export const twoBurritos = [
+  {
+    id: 1,
+    name: 'Wet Burrito',
+    image: '/images/wet-burrito.jpg',
+    desc: 'An amazing burrito!',
+    price: 5,
+    available: true,
+    spice: 1,
+  },
+  {
+    id: 2,
+    name: 'Poncho Burrito',
+    image: '/images/poncho-burrito.jpg',
+    desc: 'Maybe the second best burrito in the world!',
+    price: 7,
+    available: true,
+    spice: 2,
+  },
+];
+
+export const currentOrderItems = [
+  {
+    id: 2,
+    name: 'Poncho Burrito',
+    image: '/images/poncho-burrito.jpg',
+    desc: 'Maybe the second best burrito in the world!',
+    price: 7,
+    available: true,
+    spice: 2,
+  },
+];
